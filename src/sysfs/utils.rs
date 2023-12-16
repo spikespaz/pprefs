5317e138619e9a155dca05c79b98e56e6759a395
Jacob Birkett <jacob@birkett.dev>
2023-12-16 01:10:37 -0700

    sysfs: document unsafe

--- src/sysfs/utils.rs
+++ src/sysfs/utils.rs
@@ -7,14 +7,19 @@
 // The maximum number of bytes that can be read from any given
 // *sysfs* attribute. Generally there should be nothing larger than this.
 
+use std::fmt::Display;
 use std::fs::OpenOptions;
-use std::io::{ErrorKind, Read as _};
+use std::io::{ErrorKind, Read as _, Write as _};
 
 use super::{Result, SysfsError};
 
 pub const SYSFS_MAX_ATTR_BYTES: usize = 1024;
 
-pub(crate) fn sysfs_read<T>(file_path: &str, parse_ok: fn(&str) -> T) -> Result<T> {
+/// UNSAFE: This function assumes makes an assumption that the contents of
+/// the file at `file_path` is valid UTF-8 (in fact, it is expected to be
+/// ASCII). It is undefined behavior to use this function with file paths not
+/// exposed through *sysfs*.
+pub(crate) unsafe fn sysfs_read<T>(file_path: &str, parse_ok: fn(&str) -> T) -> Result<T> {
     let mut buf = [0; SYSFS_MAX_ATTR_BYTES];
     let result = OpenOptions::new()
         .read(true)
@@ -35,6 +40,31 @@ pub(crate) fn sysfs_read<T>(file_path: &str, parse_ok: fn(&str) -> T) -> Result<
     }
 }
 
+/// This is a low-level function which only expects that `V: Display`. It does
+/// not validate the value after formatting, so ensure that the `value` has a
+/// sufficient `Display` implementation that will produce a string appropriate
+/// for the *sysfs* attribute.
+pub(crate) fn sysfs_write<V>(file_path: &str, value: V) -> Result<()>
+where
+    V: Display,
+{
+    OpenOptions::new()
+        .read(false)
+        .write(true)
+        .create(false)
+        .open(file_path)
+        .and_then(|mut f| write!(f, "{}", value))
+        .map_err(|e| {
+            if e.kind() == ErrorKind::NotFound {
+                SysfsError::MissingAttribute
+            } else {
+                SysfsError::from(e)
+            }
+        })
+}
+
+/// UNSAFE: Path templates to `sysfs` attributes are expected to be hard-coded
+/// and validated before end-user runtime.
 macro_rules! impl_sysfs_attrs {
     () => {};
     (
@@ -57,7 +87,9 @@ macro_rules! impl_sysfs_attrs {
         $(#[$getter_meta])*
         $vis fn $attr_name ($($arg_ident : $arg_ty),*) -> $crate::sysfs::Result<$read_ty> {
             let file_path = format!("{}/{}", format_args!($sysfs_dir), stringify!($attr_name));
-            $crate::sysfs::sysfs_read::< $read_ty >(&file_path, $parse_ok)
+            unsafe {
+                $crate::sysfs::sysfs_read::< $read_ty >(&file_path, $parse_ok)
+            }
         }
 
         $crate::sysfs::impl_sysfs_attrs!($($tail)*);
