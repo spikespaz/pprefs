55c3fe094c56e5be650ab993eff1d1708d3a6b6b
Jacob Birkett <jacob@birkett.dev>
2023-12-18 21:33:31 -0700

    sysfs_cpu: rename from sysfs_cpufreq

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 // <https://github.com/torvalds/linux/blob/master/tools/power/cpupower/utils/helpers/sysfs.c>
 
-use sysfs_cpufreq as cpufreq;
+use sysfs_cpu as cpufreq;
 
 fn main() {
     for cpu_num in 0..cpufreq::num_cpus().unwrap() {
