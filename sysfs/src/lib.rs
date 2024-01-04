b4d3700d2f7a1d2f0a560a90384d9876d581e4ff
Jacob Birkett <jacob@birkett.dev>
2024-01-04 10:14:20 -0700

    sysfs: move cpu into api/cpu

--- sysfs/src/lib.rs
+++ sysfs/src/lib.rs
@@ -1,11 +1,8 @@
-#[doc(hidden)]
-pub mod cpu; // Can I get rid of the doc hidden?
-
 pub mod lib {
     pub use sysfs_lib::*;
     pub use sysfs_macros::*;
 }
 
 pub mod api {
-    pub use super::cpu;
+    pub mod cpu;
 }
