6771061cac9c7b0bd8f5f1b11e7b73da01c632ee
Jacob Birkett <jacob@birkett.dev>
2024-01-08 06:48:37 -0700

    sysfs: examples: power_supply: list all

--- sysfs/examples/power_supply.rs
+++ sysfs/examples/power_supply.rs
@@ -2,50 +2,54 @@
 mod common;
 
 use common::print_object;
+use sysfs::api::psu::list_power_supplies;
 
 fn main() {
-    print_object! {
-        in sysfs::api::psu::power_supply
-        ["/sys/class/power_supply/{}", "BAT0"] {
-            manufacturer,
-            model_name,
-            serial_number,
-            r#type,
-            current_avg,
-            current_max,
-            current_now,
-            temp,
-            temp_alert_max,
-            temp_alert_min,
-            temp_max,
-            temp_min,
-            voltage_max,
-            voltage_min,
-            voltage_now,
-            capacity,
-            capacity_alert_max,
-            capacity_alert_min,
-            capacity_error_margin,
-            capacity_level,
-            charge_control_limit,
-            charge_control_limit_max,
-            charge_control_start_threshold,
-            charge_control_end_threshold,
-            charge_type,
-            charge_term_current,
-            health,
-            precharge_current,
-            present,
-            status,
-            charge_behaviour,
-            technology,
-            voltage_avg,
-            cycle_count,
-            input_current_limit,
-            input_voltage_limit,
-            input_power_limit,
-            online,
-            usb_type,
+    for psu in list_power_supplies().iter() {
+        print_object! {
+            in sysfs::api::psu::power_supply
+            ["/sys/class/power_supply/{}", psu] {
+                manufacturer,
+                model_name,
+                serial_number,
+                r#type,
+                current_avg,
+                current_max,
+                current_now,
+                temp,
+                temp_alert_max,
+                temp_alert_min,
+                temp_max,
+                temp_min,
+                voltage_max,
+                voltage_min,
+                voltage_now,
+                capacity,
+                capacity_alert_max,
+                capacity_alert_min,
+                capacity_error_margin,
+                capacity_level,
+                charge_control_limit,
+                charge_control_limit_max,
+                charge_control_start_threshold,
+                charge_control_end_threshold,
+                charge_type,
+                charge_term_current,
+                health,
+                precharge_current,
+                present,
+                status,
+                charge_behaviour,
+                technology,
+                voltage_avg,
+                cycle_count,
+                input_current_limit,
+                input_voltage_limit,
+                input_power_limit,
+                online,
+                usb_type,
+            }
         }
+        println!();
     }
 }
