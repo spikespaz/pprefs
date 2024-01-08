9bd1f2f9998063789d028082c69c0f2e9edfcfcf
Jacob Birkett <jacob@birkett.dev>
2024-01-08 08:58:04 -0700

    sysfs: examples: add amd_pstate

---
+++ sysfs/examples/amd_pstate.rs
@@ -0,0 +1,21 @@
+#[path = "shared/common.rs"]
+mod common;
+
+use common::print_object;
+use sysfs::api::cpu::num_cpus;
+
+fn main() {
+    for cpu in 0..num_cpus().unwrap() {
+        print_object!(
+            in sysfs::api::cpu::amd_pstate
+            ["/sys/devices/system/cpu/cpufreq/policy{}", cpu] {
+                amd_pstate_highest_perf,
+                amd_pstate_max_freq,
+                amd_pstate_lowest_nonlinear_freq,
+                energy_performance_available_preferences,
+                energy_performance_preference,
+            }
+        );
+        println!();
+    }
+}
